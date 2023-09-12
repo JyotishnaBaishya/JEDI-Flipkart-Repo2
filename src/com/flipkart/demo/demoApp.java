10a2fb8e71b4a9eb5d77a3cc3749dc72f9c775cd
jyotishna <jyotishna.baishya@BLRETV-9XPGQ3VX.local>
2023-09-12 16:25:24 +0530

    my first commit

---
+++ src/com/flipkart/demo/demoApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class demoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Jyo");
+
+	}
+
+}
